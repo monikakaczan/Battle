ae2df02e1541bba9c6fbcf580d22975446d4aca3
Student <kaczanowskamonika@gmail.com>
2019-06-11 17:21:04 +0100

    Params

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
+  p params
   @names = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @names = params[:name]
   erb(:index)
 end
